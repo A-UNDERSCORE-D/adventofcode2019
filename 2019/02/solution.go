77d665678e8f48750ade0f74c0a74a16ad26804d
aunderscored <aunderscored@gmail.com>
2019-12-02 09:27:35 +0200

    added things for timing execution

--- 2019/02/solution.go
+++ 2019/02/solution.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/A-UNDERSCORE-D/adventofcode/2019/intcode"
 	"github.com/A-UNDERSCORE-D/adventofcode/util"
@@ -15,18 +16,19 @@ func main() {
 
 func part1() string {
 	input := util.GetInts(util.ReadCSV("2019/02/input.txt"))
+	t := time.Now()
 	input[1] = 12
 	input[2] = 2
 	i := intcode.IVM{Memory:input}
 	i.Execute()
-	return fmt.Sprint(i.Memory[0])
+	return fmt.Sprint(i.Memory[0], " took: ", time.Since(t))
 }
 
 func part2() string {
 	orig := util.GetInts(util.ReadCSV("2019/02/input.txt"))
 
 	var res []int
-
+	t := time.Now()
 	for a := 0; a < 100; a++ {
 		for b := 0; b < 100; b++ {
 			toRun := make([]int, len(orig))
@@ -38,7 +40,7 @@ func part2() string {
 			// out := intcode.RunIntcode(toRun)
 			if i.Memory[0] == 19690720 {
 				fmt.Printf("worked with: %d and %d\n", a, b)
-				return strconv.Itoa(100*a + b)
+				return fmt.Sprint(100*a + b, " took: ", time.Since(t))
 			}
 		}
 	}
