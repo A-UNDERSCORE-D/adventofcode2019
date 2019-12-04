bad024f74fdd99aa34a464380f0b57ded26bb90d
aunderscored <aunderscored@gmail.com>
2019-12-04 12:11:35 +0200

    added super fast solution from reddit

--- 2019/04/solution.go
+++ 2019/04/solution.go
@@ -11,23 +11,43 @@ import (
 
 func main() {
 	start := time.Now()
-	inputRange := util.GetInts(strings.Split("172930-683082", "-"))
-	t := time.Now()
-	c := part1(inputRange[0], inputRange[1])
-	fmt.Println("Part 1:", len(c), "took:", time.Since(t))
-	t = time.Now()
-	fmt.Println("Part 2:", len(part2(c)), "took:", time.Since(t))
-	fmt.Println("total time:", time.Since(start))
+		inputRange := util.GetInts(strings.Split("100000-999999", "-"))
+		t := time.Now()
+		c := part1(inputRange[0], inputRange[1])
+		fmt.Println("Part 1:", len(c), "took:", time.Since(t))
+		t = time.Now()
+		fmt.Println("Part 2:", len(part2(c)), "took:", time.Since(t))
+		fmt.Println("total time:", time.Since(start))
+		t = time.Now()
+		c1, c2 := part12(100000, 999999)
+		fmt.Println("Ultra fast:", c1, c2, time.Since(t))
 }
 
-func part1(bottom, top int) (out []int) {
-	for i := bottom; i < top; i++ {
-		strNum := strconv.Itoa(i)
-		if validateNum(strNum) {
-			out = append(out, i)
+func part12(l, h int) (out, out2 uint16) {
+	var ab, bc, cd, de, ef bool
+	for a := 1; a < 10; a++ {
+		for b := a; b < 10; b++ {
+			for c := b; c < 10; c++ {
+				for d := c; d < 10; d++ {
+					for e := d; e < 10; e++ {
+						for f := e; f < 10; f++ {
+							num := a*100_000 + b*10_000 + c*1_000 + d*100 + e*10 + f
+							if l <= num && num <= h {
+								ab, bc, cd, de, ef = a == b, b == c, c == d, d == e, e == f
+								if ab || bc || cd || de || ef {
+									out++
+								}
+								if (ab && !bc) || (!ab && bc && !cd) || (!bc && cd && !de) || (!cd && de && !ef) || (!de && ef) {
+									out2++
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
-	return
+	return out, out2
 }
 
 func validateNum(num string) bool {
@@ -43,6 +63,16 @@ func validateNum(num string) bool {
 		prev = r
 	}
 	return seenAdj
+}
+
+func part1(bottom, top int) (out []int) {
+	for i := bottom; i < top; i++ {
+		strNum := strconv.Itoa(i)
+		if validateNum(strNum) {
+			out = append(out, i)
+		}
+	}
+	return
 }
 
 func validatePt2(num string) bool {
